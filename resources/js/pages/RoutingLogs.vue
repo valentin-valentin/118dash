8edcffa98735774d74ac1dddb73200729ff65e3e
valentin-valentin <valentin.hubert@gmail.com>
2026-03-05 19:56:20 +0400

    Update RoutingLogs.vue

--- resources/js/pages/RoutingLogs.vue
+++ resources/js/pages/RoutingLogs.vue
@@ -274,7 +274,7 @@ onMounted(() => {
 
         <!-- Details Modal -->
         <Dialog :open="showDetails" @update:open="closeDetails">
-            <DialogContent class="max-w-6xl max-h-[90vh] overflow-y-auto">
+            <DialogContent class="max-w-7xl w-[90vw] max-h-[90vh] overflow-y-auto">
                 <DialogHeader>
                     <DialogTitle class="flex items-center justify-between">
                         <span>Détails du Log #{{ selectedLog?.id }}</span>
